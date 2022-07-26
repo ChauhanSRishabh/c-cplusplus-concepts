c96f8b4b244fef9aea728370b5c1ed3bd9c29dde
Rishabh Singh Chauhan <15028913+ChauhanSRishabh@users.noreply.github.com>
2022-07-26 23:18:02 +0530

    For loop

---
+++ tutorial-15.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+
+    printf("wow what a scene \n");
+    int i = 0, j = 0;
+
+    // FOR LOOP
+    for (/* i=0,j=0 */i=0;i < 5; i++, j++) // expression 1, i=0 is not needed if i is already initialized
+    {
+        printf("%d %d \n", i, j);   
+    }
+
+    return 0;
+}
